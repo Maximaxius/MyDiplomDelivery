5dae1fd67e8ae58a948a6711bcfaf8a69ac51960
Maximaxius <max.-v@mail.ru>
2024-03-10 16:53:19 +0300

    before 10.03

--- MyDiplomDelivery/Controllers/DeliverymanController.cs
+++ MyDiplomDelivery/Controllers/DeliverymanController.cs
@@ -60,12 +60,35 @@ namespace MyDiplomDelivery.Controllers
         public async Task<IActionResult> Order(string num)
         {
             ///Везде проверку по ид пользлвателя DElivManID
-            //var user = await _userManager.GetUserAsync(HttpContext.User);
+            var user = await _userManager.GetUserAsync(HttpContext.User);
             //var delivery = await _applicationContext.Delivery.Include(t => t.DeliveryDetails).Where(x => x.DeliveryManId == user.Id).ToListAsync();
-            //var orderIds = delivery.SelectMany(x =>x.DeliveryDetails).Select(x=> x.OrderId).ToList();
+            //var orderIds = delivery.SelectMany(x => x.DeliveryDetails).Select(x => x.OrderId).ToList();
             //if(!orderIds.Contains(num))
 
-            var Order = await _applicationContext.Order.FirstOrDefaultAsync(order => order.Number == num /*&& order.*/);
+            //var d = await _applicationContext.DeliveryDetail.Include(t => t.Order).Include(t => t.Delivery)
+            //    .Where(t=>t.Delivery.DeliveryManId == user.Id).Where(t=>t.Order.Number==num).ToListAsync();
+            //if (d.Count == 0)
+            //{
+            //    return RedirectToAction("Index");
+            //}
+
+            var deliveries = await _applicationContext.Delivery
+                .Include(x => x.DeliveryDetails)
+                    .ThenInclude(x => x.Order)
+                .Where(x => x.DeliveryManId == user.Id).ToListAsync();
+
+            var selectedOrders = deliveries.SelectMany(x => x.DeliveryDetails)
+                .Select(x => x.Order)
+                .ToList();
+
+            var selectedOrderNumbers = selectedOrders.Select(x=>x.Number).ToList();
+
+            if (!selectedOrderNumbers.Contains(num))
+            {
+                return RedirectToAction("Index");
+            }
+
+            var Order = await _applicationContext.Order.FirstOrDefaultAsync(order => order.Number == num );
             if (Order != null)
             {
                 DeliveryManEditOrderViewModel viewModel = new DeliveryManEditOrderViewModel
